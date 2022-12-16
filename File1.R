4628d285a176045661c4940b613e643525380e4f
Barry DeCicco <bdecicco2001@yahoo.com>
2022-12-16 10:58:53 -0500

    Start file for commands.

---
+++ File1.R
@@ -0,0 +1,26 @@
+
+# File:  File1.R.
+
+# This file is for learning how to handle missing data in R.
+
+# The first step will be running Little's Test for MCAR.
+
+####  Load Packages, set up path                    ####
+
+here::i_am(path = "inst/File_Being_Called.Rmd")
+
+library(xfun)         # for Rscript_call()
+library(devtools)     # for session_info()
+library(rstudioapi)   # for versionInfo()
+library(here)         # for here()
+library(rmarkdown)    # for render()
+library(knitr)        # for kable()
+library(lubridate)    # for today()
+library(magrittr)     # for the pipe
+library(ggplot2)      # for graphs
+
+
+#### END OF Load Packages , set up path                          ####
+
+
+
